69c87ef766dcb93817b637fea8d46f85104d9844
joshoneill19065 <joshoneill19065@gmail.com>
2020-08-03 15:50:50 -0300

    wrote readByte / writeByte functions

--- 1_axis_test/src/main.cpp
+++ 1_axis_test/src/main.cpp
@@ -1,27 +1,38 @@
 #include "mbed.h"
+
+void readRegister(int address, const char* reg, char* data);
+
 I2C i2c(PB_11, PB_10); // Data, Clock
 
 DigitalOut led1(PD_4);
 DigitalOut led2(PD_5);
 
-int address = 0x68<<1;
-char ID_reg[1]= {0x75};
+int address = 0x68;
+const char ID_reg[1] = {0x75};
 char data[1];
-
-int test = 1;
 
 int main() {
 
+    // Set Clock Frequency
     i2c.frequency(400000); 
 
-    test = i2c.write(address, ID_reg, 1,true);
-    i2c.read(address,data,1,false);
-    
+    readByte(address, ID_reg, data);
+
     if (data[0] == 0x71){
         led1.write(true); // blue
     }
     else{
         led2.write(true); // green
-        }
+    }
+}
+void readByte(int address, const char* reg, char* data){
+    i2c.write(address<<1,reg,1,true);
+    i2c.read(address<<1,data,1,false);
+    return;
+}
 
+void writeByte(int address, const char* reg, char data){
+    i2c.write(address<<1,reg,1,false);
+    i2c.write(data);
+    return;
 }
