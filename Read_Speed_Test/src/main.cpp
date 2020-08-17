9f8f51db2608d3a4566aa4a7300c2a8625fd47b0
joshoneill19065 <joshoneill19065@gmail.com>
2020-08-17 16:06:57 -0300

    debugging speed code

--- Read_Speed_Test/src/main.cpp
+++ Read_Speed_Test/src/main.cpp
@@ -2,19 +2,25 @@
 
 #define ENCODER_REVOLUTION 16 // pulse/rev
 
-int pulseCount = 0;
 
+
+volatile int pulseCount = 0;
+volatile int encoderInit = 0;
+
+DigitalOut testpin(PE_5);
+DigitalOut ledG(PD_5);
 DigitalOut ledB(PD_4);
-InterruptIn encoder(PE_2,PullDown); // GPIO Pin FFC_1_1
+InterruptIn encoder(PE_2); // GPIO Pin FFC_1_1
+
 Timer Speed_Timer;
 
 void encoder_count();
 float getSpeed();
 
+
 int main() {
-  
+
   encoder.rise(&encoder_count);
-  
   while(1) {
     if(getSpeed()>=7000){
       ledB.write(1);
@@ -26,30 +32,50 @@ int main() {
 }
 
 void encoder_count(){
-  pulseCount++;
+  ledB.write(!ledB.read());
+  encoderpulseCount++;
+  encoderInit = 1;
 }
 
 float getSpeed(){
-  int status = 0;
+  // Initialization
+  int timeout_flag = 0;
+  encoderInit = 0;
   Speed_Timer.reset();
+  
+  // Wait for the beginning of a pulse
+  while(!encoderInit);
+  
+
+  // Start the timer and initialize count
   Speed_Timer.start();
-  // Determine time for 1 revolution
-    pulseCount = 0;
-    while(1){
-      if(pulseCount >= ENCODER_REVOLUTION){
-        Speed_Timer.stop();
-        status = 1;
-        break;
-      }
-      if(Speed_Timer.read() > 1){
-        break;
-      }
+  pulseCount = 0;
+  
+  // Wait until one revolution has passed
+  while(pulseCount < ENCODER_REVOLUTION){
+    // Timeout if the motor is going too slow
+    if(Speed_Timer.read() > 1){
+      timeout_flag = 1;
+      break;
     }
-  // Calculate Speed and output
-    if(status == 1){
-      return((1.0f/Speed_Timer.read_us())*60000000); // rpm 
+  }
+  
+  // Stop timer
+  Speed_Timer.stop();
+  
+  // Return 0 if timed out
+  if(timeout_flag == 1){
+    return(0);
+  }
+  
+  // Determine Speed
+  else{
+    return((1.0f/Speed_Timer.read_us())*60000000); // rpm 
+  }
+    if(pulseCount >= ENCODER_REVOLUTION){
+      
+      status = 1; 
+      break;
     }
-    else{
-      return(0);
-    }
-}
+
+  }
