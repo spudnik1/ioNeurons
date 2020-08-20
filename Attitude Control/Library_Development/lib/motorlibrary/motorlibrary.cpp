2770fd37f65c6613c836e56209f3fbcbbfcae88d
joshoneill19065 <joshoneill19065@gmail.com>
2020-08-20 10:13:29 -0300

    resolved issue with linking src files to lib files

--- Attitude Control/Library_Development/lib/motorlibrary/motorlibrary.cpp
+++ Attitude Control/Library_Development/lib/motorlibrary/motorlibrary.cpp
@@ -1,6 +1,8 @@
-#include "motorlibrary.h"
+#include <mbed.h>
+#include <motorlibrary.h>
 
+int test_variable;
 
-    void test_function(){
-        // test_variable = 1;
-    }
+void test_function(){
+    test_variable = 1;
+}
--- Attitude Control/Library_Development/lib/motorlibrary/motorlibrary.h
+++ Attitude Control/Library_Development/lib/motorlibrary/motorlibrary.h
@@ -1,14 +1,16 @@
 #ifndef MOTOR_H
 #define MOTOR_H
 
+#include <mbed.h>
 
 // #ifndef MBED_H
 // #include <mbed.h>
 // #endif
 
-#define ENCODER_REVOLUTION 8 // pulse/rev
+// #define ENCODER_REVOLUTION 8 // pulse/rev
 
-int test_variable;
+extern int test_variable;
+
 void test_function();
 
 #endif
