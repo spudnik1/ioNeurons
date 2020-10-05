86325fa89bd405df89dcd517e7ea45dbee1b6bec
joshoneill19065 <joshoneill19065@gmail.com>
2020-10-05 15:02:12 -0300

    edited attitude model

--- Attitude Control/Library_Development/lib/Attitude_Model/Attitude_Model.cpp
+++ Attitude Control/Library_Development/lib/Attitude_Model/Attitude_Model.cpp
@@ -1,11 +1,24 @@
 #include <Attitude_Model.h>
 
+float inertia[3][3] = {{1,0,0}, {0,1,0}, {0,0,1}};
+
 // Propagating the attitude model
 void propAttitude(float* theta, float* w, float dt){
 
     float dTheta[3] = {0, 0, 0};
     float dW[3] = {0, 0, 0};
+    
+    dTheta[0] = w[0];
+    dTheta[1] = w[1];
+    dTheta[2] = w[2];
 
+    // wDot
+    
+    // c      = x(d.kWheels); % Reaction wheel rates
+    // hT     = d.inertia*w + d.inertiaRWA*(c + w);
+    // wDot   = d.inertia\(tTotal - d.tRWA + Cross(w,hT));
+    // cDot   = d.tRWA/d.inertiaRWA - wDot;
+    // xDot(d.kWheels) = cDot;
     theta[0] = 0;
     theta[1] = 0;
     theta[2] = 0;
--- Attitude Control/Library_Development/lib/Attitude_Model/Attitude_Model.h
+++ Attitude Control/Library_Development/lib/Attitude_Model/Attitude_Model.h
@@ -1,12 +1,16 @@
 #ifndef ATTMOD_H
 #define ATTMOD_H
 
+// Variables found the ADCS.h
 extern float r[3];
 extern float v[3];
 extern float quat[4];
 extern float theta[3];
 extern float w[3];
 extern float b[3];
+
+// Variables found the Attitude_Model.h
+extern float inertia[3][3];
 
 void propAttitude(float* theta, float* w, float dt);
 
